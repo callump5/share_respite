14337fd513be596f6ac364b941200f0e63b59417
Callum <clpullinger@gmail.com>
2019-11-15 02:26:20 +0000

    Contact form working

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'share_respite.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'share_settings.dev')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
