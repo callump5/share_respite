5a413b87c6c0afe6b25fa7c77faebe41d10a474e
Callum <clpullinger@gmail.com>
2019-11-15 17:01:27 +0000

    Contact form working

--- share_settings/base.py
+++ share_settings/base.py
@@ -163,7 +163,8 @@ EMAIL_USE_SSL = True
 GOOGLE_RECAPTCHA_SECRET_KEY = '6LeUusIUAAAAAECFgxNPx1KTM9eeyTkooElvdB0c'
 
 #AWS
+
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_S3_SECURE_URLS = True
+AWS_S3_SECURE_URLS = True       # use http instead of https
 AWS_QUERYSTRING_AUTH = False     # don't add complex authentication-related query parameters for requests
-AWS_STORAGE_BUCKET_NAME = 'share-respite'
+AWS_STORAGE_BUCKET_NAME = 'sharegallerymedia'
