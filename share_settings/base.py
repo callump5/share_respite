b0be6a5ec0625cfe2960838a9a4acc65d67ee609
Callum <clpullinger@gmail.com>
2019-11-16 22:51:09 +0000

    Contact form working

--- share_settings/base.py
+++ share_settings/base.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from aws import GOOGLE_RECAPTCHA_SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -166,4 +165,3 @@ AWS_QUERYSTRING_AUTH = False     # don't add complex authentication-related quer
 AWS_STORAGE_BUCKET_NAME = 'sharegallerymedia'
 
 
-GOOGLE_RECAPTCHA_SECRET_KEY = GOOGLE_RECAPTCHA_SECRET_KEY
