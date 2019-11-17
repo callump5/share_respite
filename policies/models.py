7a791ab03b7f2759703626e6977b6df1f1d11608
Callum <clpullinger@gmail.com>
2019-11-17 20:02:06 +0000

    keys

--- policies/models.py
+++ policies/models.py
@@ -2,6 +2,15 @@ from django.db import models
 
 # Create your models here.
 
+
+def upload_site_img(instance, filename):
+    filename_base, filename_ext = os.path.splitext(filename)
+    return '%s%s' % (
+        filename_base,
+        filename_ext.lower(),
+    )
+
+
 class PolicyCategory(models.Model):
     category = models.CharField(max_length=255)
 
@@ -16,7 +25,7 @@ class Policy(models.Model):
 
     title = models.CharField(max_length=255)
     category = models.ForeignKey(PolicyCategory, on_delete=models.CASCADE)
-    file = models.FileField(upload_to='uploads')
+    file = models.FileField(upload_to=upload_site_img)
 
     def __str__(self):
         return self.title
